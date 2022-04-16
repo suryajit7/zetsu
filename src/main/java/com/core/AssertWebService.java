3a6edc5a445960b4c3a3f0f91621e1f5dd0d988e
suryajit7 <suryajit7@gmail.com>
2022-04-16 20:41:12 +0530

    adding test helpers

--- src/main/java/com/core/AssertWebService.java
+++ src/main/java/com/core/AssertWebService.java
@@ -1,15 +1,28 @@
 package com.core;
 
+import com.github.fge.jsonschema.cfg.ValidationConfiguration;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import com.github.fge.jsonschema.main.JsonValidator;
 import io.restassured.response.Response;
 import lombok.SneakyThrows;
 import org.assertj.core.api.AbstractAssert;
 
-import static framework.util.file.PathFinder.getFilePathForFile;
+import static com.github.fge.jsonschema.SchemaVersion.DRAFTV4;
+import static com.util.PathFinder.getFilePathForFile;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static io.restassured.module.jsv.JsonSchemaValidator.settings;
 import static org.skyscreamer.jsonassert.JSONCompareMode.STRICT_ORDER;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class AssertWebService extends AbstractAssert<AssertWebService, Response> {
+
+    protected final ValidationConfiguration validationConfig = ValidationConfiguration.newBuilder()
+            .setDefaultVersion(DRAFTV4).freeze();
+
+    protected final JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.newBuilder()
+            .setValidationConfiguration(validationConfig).freeze();
+
+    protected JsonValidator validator = jsonSchemaFactory.getValidator();
 
     public AssertWebService(Response response) {
         super(response, AssertWebService.class);
